2f138bb08369151e4ca3bc0fcafeddf332ac9e84
Takashi Sato <ahiru154cm@gmail.com>
2023-07-11 08:57:30 +0900

    レシピテーブルを追加

--- menu_app/db/schema.rb
+++ menu_app/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_133338) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_235024) do
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "recipe_name"
+    t.string "recipe_image"
+    t.text "recipe_process"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "login_key"
     t.string "password_hash"
@@ -20,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_133338) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
