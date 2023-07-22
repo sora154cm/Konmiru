fd1f2f3183d5750359da085842bf563160f55259
Takashi Sato <ahiru154cm@gmail.com>
2023-07-22 14:06:35 +0900

    バグを一旦、整理

--- menu_app/app/controllers/recipes_controller.rb
+++ menu_app/app/controllers/recipes_controller.rb
@@ -1,12 +1,35 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user
+
+  # ログインしていなかったらログイン画面にリダイレクトする
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = "ログインが必要です"
+      redirect_to(login_path)
+    end
+  end
+  
   def new
     @user = User.find(params[:user_id])
     @recipe = @user.recipes.build
   end
 
   def create
-    
+    @recipe = Recipe.new(recipe_params)
+    respond_to do |format|
+      if @recipe.save
+        flash[:notice] = "レシピ登録が完了しました!"
+        format.html { redirect_to user_recipe_path(@recipe.user, @recipe) }
+        format.json { render json: { url: user_recipe_path(@recipe.user, @recipe) }, status: :created, location: @recipe }
+      else
+        format.html { render 'new' }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
-
+  private
+  def recipe_params
+    params.require(:recipe).permit(:user_id, :recipe_name, :recipe_image, :recipe_process, ingredients_attributes: [:id, :name, :_destroy])
+  end
 end
--- menu_app/app/models/recipe.rb
+++ menu_app/app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-
+  
   # フォームで食材を複数登録できるように設定
-  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 end
