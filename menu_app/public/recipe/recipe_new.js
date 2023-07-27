75eb0630e2166b45f52adc118a4d44e07b932cfc
Takashi Sato <ahiru154cm@gmail.com>
2023-07-27 13:23:53 +0900

    食材追加のフォームに関するバグを解決

--- menu_app/public/recipe/recipe_new.js
+++ menu_app/public/recipe/recipe_new.js
@@ -1,10 +1,28 @@
 document.addEventListener('turbo:load', function() {
+  let addButton, existingAddButton, submitButton, existingSubmitButton;
+  
+  // turbo:load イベントが発生するたびにイベントリスナーが複数回設定されてしまう問題に対処 ~~
+  // 既存の「食材を追加」ボタンのイベントハンドラを削除
+  addButton = document.querySelector('.add-ingredient-button');
+  // Remove existing handler if any
+  addButton.removeEventListener('click', addIngredientHandler); 
+  existingAddButton = addButton.cloneNode(false);
+  addButton.parentNode.replaceChild(existingAddButton, addButton);
+
+  // 既存の「レシピを登録」ボタンのイベントハンドラを削除
+  submitButton = document.getElementById('submit-button');
+  // Remove existing handler if any
+  submitButton.removeEventListener('click', submitHandler); 
+  existingSubmitButton = submitButton.cloneNode(false);
+  submitButton.parentNode.replaceChild(existingSubmitButton, submitButton);
+  // ~~ turbo:load イベントが発生するたびにイベントリスナーが複数回設定されてしまう問題に対処 
+
   // ~食材追加のフォーム追加や削除に伴う処理開始~
   function createNewIngredient(formCount) {
     // フォームを作成
     const newForm = document.createElement('input');
     newForm.type = 'text';
-    // s食材のname属性の追加
+    // 食材のname属性の追加
     newForm.name = 'ingredients[]';  
     // ingredient-inputクラスを付与
     newForm.classList.add('ingredient-input'); 
@@ -29,12 +47,24 @@ document.addEventListener('turbo:load', function() {
     // さらにingredient-additionクラスの中に子要素として入れる
     document.querySelector('.ingredient-addition').appendChild(newFormContainer);
   }
+  
+  // 「食材を追加」ボタンに対する既存のイベントハンドラを削除するため、ボタンをクローンし、元のボタンと置き換え
+  addButton = document.querySelector('.add-ingredient-button');
+  existingAddButton = addButton.cloneNode(true);
+  addButton.parentNode.replaceChild(existingAddButton, addButton);
+
   // 食材を追加ボタンを押すとフォームが追加
   document.querySelector('.add-ingredient-button').addEventListener('click', () => {
     // .ingredient-input要素の数に+1をした要素の数を取得
     const formCount = document.querySelectorAll('.ingredient-input').length + 1;
     createNewIngredient(formCount);
   });
+
+  // 「レシピを登録」ボタンに対する既存のイベントハンドラを削除するため、ボタンをクローンし、元のボタンと置き換え
+  submitButton = document.getElementById('submit-button');
+  existingSubmitButton = submitButton.cloneNode(true);
+  submitButton.parentNode.replaceChild(existingSubmitButton, submitButton);
+
   // Submitボタンをクリックしたときの動作を定義
   document.getElementById('submit-button').addEventListener('click', (event) => {
     // フォーム送信のデフォルトの動作を一旦停止
@@ -52,6 +82,13 @@ document.addEventListener('turbo:load', function() {
     // フォーム送信のデフォルトの動作を再開
     event.target.form.submit();
   });
+
+  // turbo:load イベントが発生するたびにイベントリスナーが複数回設定されてしまう問題に対処~~
+  //  食材を追加ボタンとレシピを登録ボタンに対するクリックイベントのハンドラーを設定
+  document.querySelector('.add-ingredient-button').addEventListener('click', addIngredientHandler);
+  document.getElementById('submit-button').addEventListener('click', submitHandler);
+  // ~~ turbo:load イベントが発生するたびにイベントリスナーが複数回設定されてしまう問題に対処 
+
   // ~食材追加のフォーム追加や削除に伴う処理終了~
 });
 
