76b5e8159dc6cb6829f85c110a9ef41c63de2f6c
Takashi Sato <ahiru154cm@gmail.com>
2023-08-11 22:49:50 +0900

    フォルダをアップロード

--- menu_app/db/migrate/20230711135930_add_password_digest_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordDigestToUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :password_hash, :string 
-    add_column :users, :password_digest, :string
-  end
-end
--- menu_app/db/migrate/20230730142544_add_position_to_recipe_ingredients.rb
+++
@@ -1,5 +0,0 @@
-class AddPositionToRecipeIngredients < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipe_ingredients, :position, :integer
-  end
-end
