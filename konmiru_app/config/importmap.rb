d6ed4d7060eded53979d06e9955c4fbaa1ef231d
Takashi Sato <ahiru154cm@gmail.com>
2023-08-20 11:37:56 +0900

    Javascriptを再修正

--- konmiru_app/config/importmap.rb
+++ konmiru_app/config/importmap.rb
@@ -4,8 +4,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-pin "home/index"
-pin "user/user_edit"
-pin "recipe/recipe_edit"
-pin "recipe/recipe_new"
