d9ad9b6c7e3a95bfc84b2f0ee9b28f307c52aa2b
Takashi Sato <ahiru154cm@gmail.com>
2023-09-15 16:58:53 +0900

    homeコントローラーのindexアクションを修正

--- konmiru_app/app/controllers/homes_controller.rb
+++ konmiru_app/app/controllers/homes_controller.rb
@@ -11,10 +11,8 @@ class HomesController < ApplicationController
   end
 
   def index
-    @users =User.all
     # N+1問題を解消
     @current_user.recipes = @current_user.recipes.with_attached_recipe_image
-    @recipe = Recipe.first
   end
 
   def top
